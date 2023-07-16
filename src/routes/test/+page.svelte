b0121459bfbbf716891ba02e5d79a3fc31087db1
Chris Owen <chris.j.owen@hotmail.co.uk>
2023-07-16 20:55:23 +0800

    Added foo

--- src/routes/test/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import ScreenOverview from "$lib/components/home/ScreenOverview.svelte";
-import Editor from "$lib/components/shared/editor/Editor.svelte";
-
-</script>
-
-
-<ScreenOverview />
